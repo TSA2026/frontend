358004e45d2677147fe0d14799b4ff2fbc5a7b6f
amonegro21 <monegro.ayden@outlook.com>
2026-01-03 16:14:18 -0500

    Slider test
    testing out the slider, it is VERY faulty but the basis is there.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,34 @@
 import Slider from "@react-native-community/slider";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, } from "react-native";
 
 export default function Index() {
+
+  const [value, setValue] = useState(50);
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 20,
+    },
+    slider: {
+      width: '100%',
+      height: 40,
+    },
+    text: {
+      fontSize: 18,
+      textAlign: 'center',
+      marginBottom: 20,
+    },
+  
+  });
+
   return (
-    <View className="justify-center items-center flex-1 bg-black">
-        <View className="w-[22em] h-[16em] bg-[#0f0D23] mb-[12em] rounded-full justify-center items-center">
+    <View className="items-center">
+        <View className="w-[22em] h-[16em] bg-[#0f0D23] mb-[12em] mt-[6em] rounded-full justify-center items-center">
           <Text className="color-white font-bold text-2xl">Bluetooth holder</Text>
         </View>
-        <View className="w-[22rem] h-[2.5em] bg-[#0f0D23] rounded-full mb-[16em] flex-row items-center justify-center">
+        <View className="w-[22rem] h-[2.5em] bg-[#0f0D23] rounded-full mb-[1em] flex-row items-center justify-center">
           <Text className="color-white font-bold">
             Mode 1
           </Text>
@@ -15,8 +36,19 @@ export default function Index() {
             Mode 2
           </Text>
         </View>
-        <View className="justify-center items-center flex-1">
-          <Slider className="w-[16em]" minimumValue={0} maximumValue={1} minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="#000000"/>
+        <View style={styles.container}>
+          <Text style={styles.text}>Value: {value.toFixed(0)}</Text>
+        
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={100}
+            value={value}
+            onValueChange={setValue}
+            minimumTrackTintColor="#1fb28a"
+            maximumTrackTintColor="#d3d3d3"
+            thumbTintColor="#1fb28a"
+          />
         </View>
     </View>
   );
---
+++ app/(tabs)/potato.tsx
@@ -0,0 +1,48 @@
+import Slider from '@react-native-community/slider';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const potato = () => {
+  const [value, setValue] = useState(50);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Value: {value.toFixed(0)}</Text>
+      
+      <Slider
+        style={styles.slider}
+        minimumValue={0}
+        maximumValue={100}
+        value={value}
+        onValueChange={setValue}
+        minimumTrackTintColor="#1fb28a"
+        maximumTrackTintColor="#d3d3d3"
+        thumbTintColor="#1fb28a"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  slider: {
+    width: '100%',
+    height: 40,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+
+});
+export default potato
+
+
+
+
+  
