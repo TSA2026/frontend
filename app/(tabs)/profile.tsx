1a1fc40305adba7b92e4969e7b5931587728c6c6
amonegro21 <monegro.ayden@outlook.com>
2026-01-04 12:27:00 -0500

    Log In Page (3rd)
    Log In Page basis complete. Adjustable as needed, requires backend for full completion.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 const profile = () => {
 
   const [loggedIn, setLoggedIn] = useState(false);
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+  const [isPressed, setIsPressed] = useState(false)
+
+  if (loggedIn) {
+    return (
+      <View className="bg-black flex-1">
+        <Text>potato</Text>
+      </View>
+    )
+  }
 
   return (
     <View className="flex-1 justify-center items-center bg-black">
-      <View className="w-[12em] h-[8em] justify-center items-center bg-[#0f0D23] rounded-full">
-        <Text className="color-white bg-blue-950" onPress={() => {console.log("Tried to make an account");}}>Create Account</Text>
-        <Text className="mt-2 color-white bg-blue-950" onPress={() => {console.log("Tried to log in");}}>Log In</Text>
+      <View className="w-[14em] h-[22em] rounded-lg py-2 items-center">
+        <Text className="color-white text-1xl font-semibold pb-2">To Access Settings...</Text>
+        <View className="w-full py-2">
+          <TextInput className="color-black w-full min-h-8 bg-white rounded-lg mb-1" value={emailValue} onChangeText={setEmailValue} placeholder="Email"/>
+          <TextInput className="color-black w-full min-h-8 bg-white rounded-lg" value={passwordValue} onChangeText={setPasswordValue} placeholder="Password"/>
+        </View>
+        <Pressable
+          onPress={() => console.log("You're in")}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          className={`bg-[#007AFF] py-4 px-10 rounded-xl items-center w-4/5 mt-6 ${
+            isPressed ? 'opacity-80' : 'opacity-100'
+          }`}
+        >
+          <Text className="color-white text-lg font-bold">Log In</Text>
+        </Pressable>
+        <View className="items-center">
+        <Text className="color-white w-full font-semibold mt-2">Don't have an account?</Text>
+        <Text className="color-blue-500 w-full font-semibold" onPress={() => {console.log("Signing up...")}}>Sign up!</Text>
+        </View>
       </View>
     </View>
   );
