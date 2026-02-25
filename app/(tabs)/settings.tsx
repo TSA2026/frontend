b5c400fbd71f6c8198153715d6c092cef924497e
JoseraB <josera.barran08@gmail.com>
2026-02-25 11:11:10 -0500

    modals work

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -4,6 +4,10 @@ import { Modal, Pressable, ScrollView, Text, View } from 'react-native';
 export default function profile() {
   const [aboutModalVisible, setAboutModalVisible] = useState(false);
   const [pressedItem, setPressedItem] = useState<string | null>(null);
+  const [accessibilityModalVisible, setAccessibilityModalVisible] = useState(false);
+  const [customModalVisible, setCustomModalVisible] = useState(false);
+  const [accountModalVisible, setAccountModalVisible] = useState(false);
+
 
   return (
     <ScrollView className='bg-black '>
@@ -20,36 +24,39 @@ export default function profile() {
           borderBottomColor: pressedItem === 'Account' ? '#7C3AED' : '#262626'}} 
         onPressIn={() => setPressedItem('Account')}
        onPressOut={() => setPressedItem(null)}
-        onPress={() => console.log("Account Pressed")}>
+        onPress={() => {console.log("Account Pressed")
+          setAccountModalVisible(true);}}>
 
           <Text className="color-white text-2xl font-semibold">Account</Text>
           <Text className="color-white ">{'>'}</Text>
         </Pressable>
+        
+        
         <Pressable 
         className="flex-row justify-between pt-4 pb-4 items-center"
         style={{ borderBottomWidth: 1, 
           borderBottomColor: pressedItem === 'Custom' ? '#7C3AED' : '#262626'}} 
           onPressIn={() => setPressedItem('Custom')}
           onPressOut={() => setPressedItem(null)}
-          onPress={() => console.log("Custom Pressed")}>
+          onPress={() => {console.log("Custom Pressed")
+            setAccessibilityModalVisible(true);}}>
 
           <Text className="color-white text-2xl font-semibold">Custom Modes</Text>
           <Text className="color-white ">{'>'}</Text>
         </Pressable>
         
         
-        <Pressable 
-        className="flex-row justify-between pt-4 pb-4 items-center"
-        style={{ borderBottomWidth: 1, 
-          borderBottomColor: pressedItem === 'Accessibility' ? '#7C3AED' : '#262626'}} 
-           onPressIn={() => setPressedItem('Accessibility')}
-           onPressOut={() => setPressedItem(null)}
-        
-        onPress={() => console.log("Accessibility Pressed")}>
-          <Text className="color-white text-2xl font-semibold">Accessibility</Text>
-          <Text className="color-white">{'>'}</Text>
+        <Pressable
+          className="flex-row justify-between pt-4 pb-4 items-center"
+          style={{ borderBottomWidth: 1, borderBottomColor: pressedItem === 'Accessibility' ? '#7C3AED' : '#262626' }}
+          onPressIn={() => setPressedItem('Accessibility')}
+          onPressOut={() => setPressedItem(null)}
+          onPress={() => {console.log("Accessibility Pressed");setAccessibilityModalVisible(true);}}
+            >
+            <Text className="color-white text-2xl font-semibold">Accessibility</Text>
+            <Text className="color-white">{'>'}</Text>
         </Pressable>
-        
+      
         
         <Pressable 
         
@@ -64,6 +71,30 @@ export default function profile() {
         </Pressable>
       </View>
     </View>
+      
+    <Modal
+         animationType='slide' 
+        transparent={true} 
+        visible={accountModalVisible} 
+        onRequestClose={() => {setAccountModalVisible(false)}}>
+          
+        <View className="flex-1 justify-center items-center mt-[8em] "
+        style={{ pointerEvents: 'box-none' }}>
+          <View className='rounded-lg w-[15em] h-[12em] bg-black'>
+            <Text className="color-white p-2">
+              This is a software that automatically adjust audio dependent on the selected mode. Lots of fine tuning had gone into this.
+            </Text>
+            <Pressable onPress={() => {setAccountModalVisible(false)}}>
+              <Text className='color-[#85FF00] text-center'>
+                Close
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+
+
       <Modal animationType='slide' transparent={true} visible={aboutModalVisible} onRequestClose={() => {setAboutModalVisible(!aboutModalVisible)}}>
         <View className="flex-1 justify-center items-center mt-[8em]">
           <View className='rounded-lg w-[15em] h-[12em] bg-black'>
@@ -78,6 +109,54 @@ export default function profile() {
           </View>
         </View>
       </Modal>
+
+
+
+      <Modal
+         animationType='slide' 
+        transparent={true} 
+        visible={customModalVisible} 
+        onRequestClose={() => {setCustomModalVisible(false)}}>
+          
+        <View className="flex-1 justify-center items-center mt-[8em] "
+        style={{ pointerEvents: 'box-none' }}>
+          <View className='rounded-lg w-[25em] h-[40em] bg-black'>
+            <Text className="color-white p-2">
+              This is a software that automatically adjust audio dependent on the selected mode. Lots of fine tuning had gone into this.
+            </Text>
+            <Pressable onPress={() => {setCustomModalVisible(false)}}>
+              <Text className='color-[#85FF00] text-center'>
+                Close
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+ 
+
+
+
+      <Modal
+         animationType='slide' 
+        transparent={true} 
+        visible={accessibilityModalVisible} 
+        onRequestClose={() => {setAccessibilityModalVisible(false)}}>
+          
+        <View className="flex-1 justify-center items-center mt-[8em] "
+        style={{ pointerEvents: 'box-none' }}>
+          <View className='rounded-lg w-[15em] h-[12em] bg-black'>
+            <Text className="color-white p-2">
+              This is a software that automatically adjust audio dependent on the selected mode. Lots of fine tuning had gone into this.
+            </Text>
+            <Pressable onPress={() => {setAccessibilityModalVisible(false)}}>
+              <Text className='color-[#85FF00] text-center'>
+                Close
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+ 
     </ScrollView>
   )
 }
